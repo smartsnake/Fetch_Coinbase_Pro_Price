5a08f24800dd2120aeeb875562fb9d0ad4568633
Brian Dearing <smartsnake1@gmail.com>
2019-12-28 20:59:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import cbpro
-public_client = cbpro.PublicClient()
+from pymongo import MongoClient
+from datetime import datetime
+import time
+from os import path
+import json
 
-public_client.get_product_24hr_stats('BTC-USD')
+with open('config.json') as json_data_file:
+    data = json.load(json_data_file)
+print(data)
+
+public_client = cbpro.PublicClient()
+client = MongoClient(data['mongo_url'], int(data['mongo_port']))
+
+db = client[data['database']]
+collection = db[data['collection']]
+
+while(not path.exists(data['end_file'])):
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    Doc = public_client.get_product_24hr_stats(data['currency'])
+    Doc.update({'timestamp' : timestamp})
+    print(Doc)
+    collection.insert_one(Doc)
+    time.sleep(60)
